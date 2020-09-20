db352ed21651e9202cb8a0be28e0ef46db8db163
spcidealacm <spcidealacm@gmail.com>
2020-09-21 01:07:53 +1000

    jquery

--- js/index.js
+++ js/index.js
@@ -1,20 +1,31 @@
-function loadImage(id, targetId) {
-    console.log(1);
-    let element = document.getElementById(id);
-    let targetElement = targetId ? document.getElementById(targetId) : element;
-    if (element.dataset.image) {
-        console.log(2);
-        let img = new Image();
-        img.src = element.dataset.image;
-        console.log("img.src: ", img.src);
-        img.onload = function () {
-            targetElement.classList.add('is-loaded');
+(function ($) {
+    "use strict"
+    $(".realbody").scroll(function () {
+        let scroll = $(".realbody").scrollTop();
+        let result = Math.floor(scroll / 200) / 20;
+        if (scroll < 800) {
+            $(".navheader").css("background-color", `rgba(255, 255, 255, ${result})`);
+            $(".navheader").css("box-shadow", `0px 5px 5px rgba(0, 0, 0, ${result})`);
+        }
+    });
+
+    function loadImage(id, targetId) {
+        let element = document.getElementById(id);
+        let targetElement = targetId ? document.getElementById(targetId) : element;
+        if (element.dataset.image) {
+            let img = new Image();
+            img.src = element.dataset.image;
+            img.onload = function () {
+                targetElement.classList.add('is-loaded');
+            }
         }
     }
-}
 
-document.addEventListener('DOMContentLoaded', event => {
-    loadImage('wallpaper');
-    loadImage('pictureImage', 'picture');
-});
+    document.addEventListener('DOMContentLoaded', event => {
+        loadImage('wallpaper');
+        loadImage('pictureImage', 'picture');
+    });
+
+
+})(jQuery);
 
