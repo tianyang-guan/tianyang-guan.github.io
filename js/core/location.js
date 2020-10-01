cd9582e5c59afbbb60ddd30111ce4604144ce00b
spcidealacm <spcidealacm@gmail.com>
2020-10-01 18:24:45 +1000

    Update location.js

--- js/core/location.js
+++ js/core/location.js
@@ -47,7 +47,7 @@ function initMap() {
 
 async function sendPos(pos) {
     let response = await fetch(
-        `http://websvf.top:3000/position/${pos.lat}/${pos.lng}`
+        `https://websvf.top:3000/position/${pos.lat}/${pos.lng}`
     );
     let result = response.json();
     console.log(pos);
