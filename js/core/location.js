15efc6fd27831f451d65fa3d721d74f9cf905b86
spcidealacm <spcidealacm@gmail.com>
2020-10-01 13:58:54 +1000

    google map

---
+++ js/core/location.js
@@ -0,0 +1,48 @@
+let map, infoWindow;
+
+let pos;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 15,
+    });
+    infoWindow = new google.maps.InfoWindow();
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+
+                let latLng = new google.maps.LatLng(pos.lat, pos.lng);
+                let marker = new google.maps.Marker({
+                    position: latLng,
+                    map: map,
+                    title: "Your location",
+                });
+
+                map.setCenter(pos);
+            },
+            () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+            }
+        );
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation
+            ? "Error: The Geolocation service failed."
+            : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+}
