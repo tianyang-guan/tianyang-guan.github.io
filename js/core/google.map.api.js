7fd542906e6151485f15469028bebd0b63457b60
spcidealacm <spcidealacm@gmail.com>
2020-10-03 17:44:24 +1000

    Update google.map.api.js

--- js/core/google.map.api.js
+++ js/core/google.map.api.js
@@ -2,7 +2,7 @@
 var script = document.createElement("script");
 script.src =
     "https://maps.googleapis.com/maps/api/js?key=AIzaSyD3KXgxFtMXwJlKW4xp5CDHieNQt-x-x1Q&callback=initMap";
-script.defer = true;
+script.defer = false;
 
 // Attach your callback function to the `window` object
 window.initMap = function () {
